dca78c500b7d9d00253d1bacf3d8095175db93c4
shrugr <s.shruthi89@gmail.com>
2016-04-03 13:10:32 -0400

    portfolio iteration 2

--- jinjatemplates2/main.py
+++ jinjatemplates2/main.py
@@ -30,14 +30,14 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class IndexHandler(webapp2.RequestHandler):
     def get(self):
     	template = JINJA_ENVIRONMENT.get_template('templates/index.html')
-    	self.response.write(template.render({'title': 'HOME','subheading': 'HOME','homenav': 'HOME', 'famnav':'Family','foodnav':'Food','loginnav':'Login'}))
+    	self.response.write(template.render({'title': 'HOME','subheading': 'HOME','homenav': 'HOME', 'revnav':'Reviews','foodnav':'Food','loginnav':'Login'}))
 
 class FamilyHandler(webapp2.RequestHandler):
     def get(self):
         logthepathfam=self.request.path
         logging.info('the path for family is:'+logthepathfam)
     	template = JINJA_ENVIRONMENT.get_template('templates'+logthepathfam)#path not hardcoded
-    	self.response.write(template.render({'title': 'FAMILY','subheading':'FAMILY','famnav':'FAMILY', 'homenav': 'Home','foodnav':'Food','loginnav':'Login'}))
+    	self.response.write(template.render({'title': 'REVIEWS','subheading':'REVIEWS','revnav':'REVIEWS', 'homenav': 'Home','foodnav':'Food','loginnav':'Login'}))
 
 
 
@@ -46,13 +46,13 @@ class FoodHandler(webapp2.RequestHandler):
         logthepathfood=self.request.path
         logging.info('the path for food is:'+logthepathfood)
     	template = JINJA_ENVIRONMENT.get_template('templates'+logthepathfood)#path not hardcoded
-    	self.response.write(template.render({'title': 'FOOD','subheading':'FOOD','famnav':'Family', 'homenav': 'Home','foodnav':'FOOD','loginnav':'Login'}))
+    	self.response.write(template.render({'title': 'FOOD','subheading':'FOOD','revnav':'Reviews', 'homenav': 'Home','foodnav':'FOOD','loginnav':'Login'}))
 
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
         msgdok=""
         template = JINJA_ENVIRONMENT.get_template('templates/login.html')
-        self.response.write(template.render({'title': 'LOGIN','subheading':'LOGIN','msg':msgdok, 'famnav':'Family', 'homenav': 'Home','foodnav':'Food','loginnav':'LOGIN'}))
+        self.response.write(template.render({'title': 'LOGIN','subheading':'LOGIN','msg':msgdok, 'revnav':'Reviews', 'homenav': 'Home','foodnav':'Food','loginnav':'LOGIN'}))
     def post(self):
         logun=self.request.get('un')
         logp=self.request.get('p')
@@ -62,18 +62,18 @@ class LoginHandler(webapp2.RequestHandler):
             logging.info('correct user name:'+logun)
             logging.info('correct password:'+logp)
             template = JINJA_ENVIRONMENT.get_template('templates/loggedin.html')
-            self.response.write(template.render({'title': 'LOGGED IN','subheading':'LOGGED IN..','famnav':'Family', 'homenav': 'Home','foodnav':'Food','loginnav':'LOGIN'}))
+            self.response.write(template.render({'title': 'LOGGED IN','subheading':'LOGGED IN..','revnav':'Reviews', 'homenav': 'Home','foodnav':'Food','loginnav':'LOGIN'}))
         else:
             logging.info('incoreect username:'+logun)
             logging.info('incorrect password:'+logp)
             msgdbad="Bad credentials, please try again"
             template = JINJA_ENVIRONMENT.get_template('templates/login.html')
-            self.response.write(template.render({'title': 'LOGIN','subheading':'LOGIN','msg':msgdbad, 'famnav':'Family', 'homenav': 'Home','foodnav':'Food','loginnav':'LOGIN'}))
+            self.response.write(template.render({'title': 'LOGIN','subheading':'LOGIN','msg':msgdbad, 'revnav':'Reviews', 'homenav': 'Home','foodnav':'Food','loginnav':'LOGIN'}))
     
 app = webapp2.WSGIApplication([
     ('/', IndexHandler),
     ('/index.html', IndexHandler),
-    ('/family.html', FamilyHandler),
+    ('/reviews.html', FamilyHandler),
     ('/food.html', FoodHandler),
     ('/log.*',LoginHandler)
 ], debug=True)
